a4c96aeb1c40b340ce24e0079d431cc15f221b9b
sajeewa9930 <sajeewaml@gmail.com>
2026-01-13 11:54:48 +0530

    Add Table to Inventory/products

--- app/blueprints/inventory/__init__.py
+++ app/blueprints/inventory/__init__.py
@@ -1,5 +1,15 @@
 from flask import Blueprint
 
-inventory_bp = Blueprint('inventory', __name__)
+inventory_bp = Blueprint('inventory', __name__, url_prefix='/inventory')
 
-from . import dashboard, products, suppliers, purchase_orders, grn
+# Import all routes
+from . import (
+    dashboard,
+    products,
+    purchase_orders,
+    suppliers,
+    grn
+)
+
+# Make sure all routes are imported
+__all__ = ['inventory_bp']
